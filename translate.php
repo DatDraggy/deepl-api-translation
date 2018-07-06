f6c1967d48d17c712c6241547f3b6e538e1ac785
DatDraggy <spamme@kieran.pw>
2018-07-06 10:39:22 +0200

    fix

--- translate.php
+++ translate.php
@@ -20,11 +20,12 @@ $handle = fopen($inFile, "r");
 if ($handle) {
   while (($line = fgets($handle)) !== false) {
     $lineArr = explode(';', $line);
-    $text = $lineArr[2];
+    $text = urlencode(str_replace('"', '', $lineArr[2]));
 
-    $jsonDec = json_decode(file_get_contents($baseUrl . "text=$text&source_lang=$sourceLang&target_lang=$targetLang&auth_key=$authKey"), TRUE);
-    $translation = $jsonDec['translations']['text'];
-
-    file_put_contents($outFile, $lineArr[0] . ';' . $lineArr[1] . ';' . $translation . "\n", FILE_APPEND | LOCK_EX);
+    $json = file_get_contents($baseUrl . "text=$text&source_lang=$sourceLang&target_lang=$targetLang&auth_key=$authKey");
+    $jsonDec = json_decode($json, TRUE);
+    $translation = preg_replace( "/\r|\n/", "", $jsonDec['translations'][0]['text']);
+    file_put_contents($outFile, $lineArr[0] . ';' . $lineArr[1] . ';"' . $translation . '"' . "\n", FILE_APPEND | LOCK_EX);
+    echo $translation . "\n";
   }
 }
